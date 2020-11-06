c0ea9b172f26746dfbfc957fa069ca0ee349a1f1
addrianyy <adrianvpl@gmail.com>
2020-11-06 21:09:43 +0100

    update all passes

--- turbo_ir/src/passes/simplify_cfg.rs
+++ turbo_ir/src/passes/simplify_cfg.rs
@@ -20,7 +20,6 @@ impl super::Pass for SimplifyCFGPass {
         // ... body ...
         //
         // If label_1 is reachable only from label_0 this will get optimized to:
-        //
         // label_0:
         // ... body ...
 
@@ -29,39 +28,39 @@ impl super::Pass for SimplifyCFGPass {
             let outgoing = function.flow_graph_outgoing();
 
             for (&target, predecessors) in &incoming {
-                // Don't do anything if it's entry block or if it has more then 1 entry point.
+                // Don't do anything if it's entry block or if it has more than 1 entry point.
                 if target == Label(0) || predecessors.len() != 1 {
                     continue;
                 }
 
-                // Get the parent of current block and make sure it's actually predecessor.
+                // Get the parent of the current block and make sure it's not a loop.
                 let dominator = *predecessors.iter().next().unwrap();
                 if  dominator == target {
                     continue;
                 }
 
-                // We can only merge if current block is entered by a non-conditional branch.
+                // We can only merge if current block is entered to by a non-conditional branch.
                 if !matches!(function.last_instruction(dominator), Instruction::Branch { .. }) {
                     continue;
                 }
 
-                // Merged block shouldn't contain PHIs. If they do, another optimization pass
-                // will remove it and we will be able to merge block later.
+                // Merged block shouldn't contain PHIs. If it does, another optimization pass
+                // will remove it and we will be able to merge it later.
                 if function.block_contains_phi(target) {
                     continue;
                 }
 
-                // All successors PHI nodes should now refer to `dominator` instead of `target`.
+                // All conditions met: we can merge target into dominator.
+
+                // Successors' PHI nodes should now refer to `dominator` instead of `target`.
                 for successor in &outgoing[&target] {
                     function.replace_phi_incoming(*successor, target, dominator);
                 }
 
-                // All conditions met: we can merge target into dominator.
-
                 let mut target_insts = function.blocks.remove(&target).unwrap();
                 let dominator_insts  = function.blocks.get_mut(&dominator).unwrap();
 
-                // Remove branch to target in dominator.
+                // Remove branch to target in the dominator.
                 dominator_insts.pop().unwrap();
 
                 // Move all instructions from target to the end of the dominator.
@@ -77,10 +76,10 @@ impl super::Pass for SimplifyCFGPass {
         }
 
         // Flatten control flow. If some instruction branches to a block which only contains
-        // non-conditional branch then we can get rid of that intermediate jump. 
+        // non-conditional branch then we can get rid of that intermediate jump.
         //
         // label_0:
-        //  bcond %0, label_1, label_2
+        //  bcond v0, label_1, label_2
         //
         // label_1:
         //  branch label_3
@@ -93,7 +92,7 @@ impl super::Pass for SimplifyCFGPass {
         //
         // Will get optimized to:
         // label_0:
-        //  bcond %0, label_3, label_2
+        //  bcond v0, label_3, label_2
         // 
         // ......
 
@@ -122,72 +121,74 @@ impl super::Pass for SimplifyCFGPass {
 
         // Do a second pass to actually flatten CFG.
         for &label in &labels {
-            let mut body  = &function.blocks[&label];
-            let block_len = body.len();
+            // Skip intermediate labels themselves.
+            if branch_labels.get(&label).is_some() {
+                continue;
+            }
 
-            for inst_id in 0..block_len {
-                let mut phi_updates = Vec::new();
-                let mut replacement = None;
+            let body    = &function.blocks[&label];
+            let inst_id = body.len() - 1;
 
-                // Find branch instructions which target intermediate blocks and change their
-                // target to real destination.
-                match body[inst_id] {
-                    Instruction::Branch { target } => {
-                        if let Some(&new_target) = branch_labels.get(&target) {
-                            // Check if PHI instruction depends on the control flow.
-                            if !function.depends_on_predecessors(new_target, &[label, target]) {
-                                replacement = Some(Instruction::Branch { 
-                                    target: new_target,
-                                });
+            let mut phi_updates = Vec::new();
+            let mut replacement = None;
 
-                                phi_updates.push((target, new_target));
-                            }
-                        }
-                    }
-                    Instruction::BranchCond { cond, on_true, on_false } => {
-                        let mut changed_true  = on_true;
-                        let mut changed_false = on_false;
-
-                        if let Some(&new_true) = branch_labels.get(&on_true) {
-                            // Check if PHI instruction depends on the control flow.
-                            if !function.depends_on_predecessors(new_true, &[label, on_true]) {
-                                changed_true = new_true;
-                                phi_updates.push((on_true, new_true));
-                            }
-                        }
-
-                        if let Some(&new_false) = branch_labels.get(&on_false) {
-                            // Check if PHI instruction depends on the control flow.
-                            if !function.depends_on_predecessors(new_false, &[label, on_false]) {
-                                changed_false = new_false;
-                                phi_updates.push((on_false, new_false));
-                            }
-                        }
-
-                        // Create replacement instruction if we have changed anything.
-                        if changed_true != on_true || changed_false != on_false {
-                            replacement = Some(Instruction::BranchCond {
-                                cond,
-                                on_true:  changed_true,
-                                on_false: changed_false,
+            // Find branch instructions which target intermediate blocks and change their
+            // target to real destination.
+            match body[inst_id] {
+                Instruction::Branch { target } => {
+                    if let Some(&new_target) = branch_labels.get(&target) {
+                        // Check if PHI instruction depends on the control flow.
+                        if !function.depends_on_predecessors(new_target, &[label, target]) {
+                            replacement = Some(Instruction::Branch {
+                                target: new_target,
                             });
+
+                            phi_updates.push((target, new_target));
                         }
                     }
-                    _ => {}
                 }
+                Instruction::BranchCond { cond, on_true, on_false } => {
+                    let mut changed_true  = on_true;
+                    let mut changed_false = on_false;
 
-                if let Some(replacement) = replacement {
-                    // Fixup PHI incoming values.
-                    for (before, after) in phi_updates {
-                        function.replace_phi_incoming(after, before, label);
+                    if let Some(&new_true) = branch_labels.get(&on_true) {
+                        // Check if PHI instruction depends on the control flow.
+                        if !function.depends_on_predecessors(new_true, &[label, on_true]) {
+                            changed_true = new_true;
+                            phi_updates.push((on_true, new_true));
+                        }
                     }
 
-                    // Replace instruction with our new one.
-                    *function.instruction_mut(Location::new(label, inst_id)) = replacement;
+                    if let Some(&new_false) = branch_labels.get(&on_false) {
+                        // Check if PHI instruction depends on the control flow.
+                        if !function.depends_on_predecessors(new_false, &[label, on_false]) {
+                            changed_false = new_false;
+                            phi_updates.push((on_false, new_false));
+                        }
+                    }
 
-                    body          = &function.blocks[&label];
-                    did_something = true;
+                    // Create replacement instruction if we have changed anything.
+                    if changed_true != on_true || changed_false != on_false {
+                        replacement = Some(Instruction::BranchCond {
+                            cond,
+                            on_true:  changed_true,
+                            on_false: changed_false,
+                        });
+                    }
                 }
+                _ => {}
+            }
+
+            if let Some(replacement) = replacement {
+                // Fixup PHI incoming values.
+                for (before, after) in phi_updates {
+                    function.replace_phi_incoming(after, before, label);
+                }
+
+                // Replace instruction with our new one.
+                *function.instruction_mut(Location::new(label, inst_id)) = replacement;
+
+                did_something = true;
             }
         }
 
--- turbo_ir/src/passes/simplify_compares.rs
+++ turbo_ir/src/passes/simplify_compares.rs
@@ -13,24 +13,24 @@ impl super::Pass for SimplifyComparesPass {
 
         let mut replacements = Vec::new();
 
-        // Code generators will often emit sequence of icmp, select, icmp.
-        // This pass will try to detect it and optimize it to a single icmp if possible.
+        // Code generators will often emit sequence of `icmp`, `select`, `icmp`.
+        // This pass will try to detect it and optimize it to a single `icmp` if possible.
         //
-        // %2 = u32 0
-        // %4 = icmp eq u32 %0, %2
-        // %5 = u8 0
-        // %6 = u8 1
-        // %7 = select u1 %4, u8 %6, %5
-        // %9 = icmp ne u8 %7, %5
-        // bcond u1 %9, label_2, label_3
+        // v2 = u32 0
+        // v4 = icmp eq u32 v0, v2
+        // v5 = u8 0
+        // v6 = u8 1
+        // v7 = select u1 v4, u8 v6, v5
+        // v9 = icmp ne u8 v7, v5
+        // bcond u1 v9, label_2, label_3
         //
         // Gets optimized to:
-        // %2 = u32 0
-        // %4 = icmp eq u32 %0, %2
-        // bcond u1 %4, label_2, label_3
+        // v2 = u32 0
+        // v4 = icmp eq u32 v0, v2
+        // bcond u1 v4, label_2, label_3
 
         function.for_each_instruction(|location, instruction| {
-            // Try to match on SECOND icmp of icmp, select, icmp sequence.
+            // Try to match on SECOND `icmp` of `icmp`, `select`, `icmp` sequence.
             if let Instruction::IntCompare { dst, a, pred, b } = instruction {
                 let mut a = a;
                 let mut b = b;
@@ -40,8 +40,8 @@ impl super::Pass for SimplifyComparesPass {
                 // order doesn't matter.
 
                 for _ in 0..2 {
-                    // Left side of icmp must be a value created by select instruction.
-                    // Right side of icmp must be a known constant.
+                    // Left side of `icmp` must be a value created by `select` instruction.
+                    // Right side of `icmp` must be a known constant.
                     let aa = creators.get(&a).map(|location| function.instruction(*location));
                     let bb = consts.get(&b);
 
@@ -51,7 +51,7 @@ impl super::Pass for SimplifyComparesPass {
                         let new_on_true;
                         let new_on_false;
 
-                        // Select operands must be known constants too.
+                        // `select` operands must be known constants too.
                         if let (Some((_, on_true)), Some((_, on_false))) = (consts.get(on_true),
                                                                             consts.get(on_false)) {
                             new_on_true  = on_true;
@@ -63,18 +63,19 @@ impl super::Pass for SimplifyComparesPass {
                         let on_true  = new_on_true;
                         let on_false = new_on_false;
 
-                        // If select true value is the same as false value then select instruction
-                        // can be optimized out. Other optimization pass will take care of it.
+                        // If select true value is the same as false value then `select`
+                        // instruction can be optimized out. Other optimization pass
+                        // will take care of it.
                         if on_true == on_false {
                             return;
                         }
 
-                        // Get the corelation betwen first icmp and second icmp.
-                        // There can be 3 cases.
-                        // 1. second icmp result ==  first icmp result.
-                        // 2. second icmp result == !first icmp result.
-                        // 3. icmps are not corelated (in this case we exit).
-                        // For simplicity we only handle EQ and NE predicates in the second icmp.
+                        // Get the corelation betwen first `icmp` and second `icmp`.
+                        // There can be 3 cases:
+                        // 1. second `icmp` result ==  first `icmp` result.
+                        // 2. second `icmp` result == !first `icmp` result.
+                        // 3. `icmps` are not corelated (in this case we exit).
+                        // For simplicity we only handle EQ and NE predicates in the second `icmp`.
 
                         let result = match *pred {
                             IntPredicate::Equal => {
@@ -98,22 +99,22 @@ impl super::Pass for SimplifyComparesPass {
                             _ => return,
                         };
 
-                        // We know that both icmps are corelated with each other.
+                        // We know that both `icmps` are corelated with each other.
 
                         let mut new_instruction = None;
 
                         if let Some(inverted) = result {
                             if !inverted {
-                                // If second icmp result == first icmp result than
-                                // we will just alias second icmp result to first one's result.
+                                // If second `icmp` result == first `icmp` result than
+                                // we will just alias second `icmp` result to first one's result.
 
                                 new_instruction = Some(Instruction::Alias { 
                                     dst:   *dst,
                                     value: *cond,
                                 });
                             } else {
-                                // If icmp results are inverted than we want to replace second 
-                                // icmp with inverted copy of the first one.
+                                // If `icmp` results are inverted than we want to replace second
+                                // `icmp` with inverted copy of the first one.
                                 let parent_compare = creators.get(&cond).map(|location| {
                                     function.instruction(*location)
                                 });
@@ -124,7 +125,7 @@ impl super::Pass for SimplifyComparesPass {
                                     let mut a = a;
                                     let mut b = b;
 
-                                    // Invert icmp instruction.
+                                    // Invert `icmp` instruction.
                                     let (new_pred, needs_swap) = match pred {
                                         IntPredicate::Equal    => (IntPredicate::NotEqual, false),
                                         IntPredicate::NotEqual => (IntPredicate::Equal,    false),
@@ -145,7 +146,8 @@ impl super::Pass for SimplifyComparesPass {
                                         std::mem::swap(&mut a, &mut b);
                                     }
 
-                                    // Change this instruction to inverted version of first icmp.
+                                    // Change this instruction to inverted version of the
+                                    // first `icmp`.
                                     new_instruction = Some(Instruction::IntCompare {
                                         dst: *dst,
                                         a,
